c8e8915ee48fb8a7165f1bdd6ea83e1ab15046ae
gaigutherz <gaigutherz@gmail.con>
2020-05-19 23:20:59 +0300

    Make hmm, memm and biLSTM independent of each other

--- src/train.py
+++ src/train.py
@@ -8,69 +8,66 @@ from allennlp.predictors import SentenceTaggerPredictor
 
 
 def hmm_train_and_store():
-    train_texts, dev_texts, test_texts, sign_to_id, tran_to_id, id_to_sign, id_to_tran = preprocess()
+    train_texts, dev_texts, test_texts, _, _, _, _ = preprocess()
     most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2 = \
         hmm_train(train_texts, dev_texts)
 
     dump_object_to_file((most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1,
-                         lambda2, sign_to_id, tran_to_id, id_to_sign, id_to_tran), r"..\output\hmm_model")
-
-    return train_texts, dev_texts, test_texts
-
-
-def memm_and_hmm_train_and_store():
-    train_texts, dev_texts, test_texts = hmm_train_and_store()
-
-    memm_train(train_texts, dev_texts)
-    logreg, vec, idx_to_tag_dict = memm_train(train_texts, dev_texts)
-    extra_decoding_arguments = build_extra_decoding_arguments(train_texts)
-
-    dump_object_to_file((logreg, vec, idx_to_tag_dict, extra_decoding_arguments), r"..\output\memm_model")
+                         lambda2), Path(r"../output/hmm_model"))
 
     return test_texts
 
 
-def biLSTM_memm_and_hmm_train_and_store():
-    test_texts = memm_and_hmm_train_and_store()
+def hmm_train_and_test():
+    hmm_test_texts = hmm_train_and_store()
+
+    most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2 = \
+        load_object_from_file(Path(r"../output/hmm_model.pkl"))
+    print(compute_accuracy(hmm_test_texts, hmm_viterbi, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2))
+
+
+def memm_train_and_store():
+    train_texts, dev_texts, test_texts, _, _, _, _ = preprocess()
+
+    logreg, vec, idx_to_tag_dict = memm_train(train_texts, dev_texts)
+    extra_decoding_arguments = build_extra_decoding_arguments(train_texts)
+
+    dump_object_to_file((logreg, vec, idx_to_tag_dict, extra_decoding_arguments), Path(r"../output/memm_model"))
+
+    return test_texts
+
+
+def memm_train_and_test():
+    memm_test_texts = memm_train_and_store()
+
+    logreg, vec, idx_to_tag_dict, extra_decoding_arguments = load_object_from_file(Path("../output/memm_model.pkl"))
+    print(compute_accuracy(memm_test_texts, memm_greedy, logreg, vec, idx_to_tag_dict))
+
+
+def biLSTM_train_and_store():
+    train_texts, dev_texts, test_texts, sign_to_id, _, _, id_to_tran = preprocess()
 
     model, vocab, train_dataset, validation_dataset, cuda_device, reader = prepare1()
     trainer, model, reader, vocab = prepare2(model, vocab, train_dataset, validation_dataset, cuda_device, reader)
     trainer.train()
     predictor = SentenceTaggerPredictor(model, dataset_reader=reader)
 
-    dump_object_to_file((model, predictor), r"..\output\biLSTM_model")
+    dump_object_to_file((model, predictor, sign_to_id, id_to_tran), Path(r"../output/biLSTM_model"))
 
     return test_texts
 
 
-def load_trained_data():
-    most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2, sign_to_id, \
-    tran_to_id, id_to_sign, id_to_tran = load_object_from_file(Path("../output/hmm_model.pkl"))
+def biLSTM_train_and_test():
+    biLSTM_test_texts = biLSTM_train_and_store()
 
-    logreg, vec, idx_to_tag_dict, extra_decoding_arguments = load_object_from_file(Path("../output/memm_model.pkl"))
-
-    model, predictor = load_object_from_file(Path("../output/biLSTM_model.pkl"))
-
-    return most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2, \
-           sign_to_id, tran_to_id, id_to_sign, id_to_tran, logreg, vec, idx_to_tag_dict, extra_decoding_arguments, \
-           model, predictor
-
-
-def test_trained_data(test_texts, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2, logreg, vec,
-                      idx_to_tag_dict, model, predictor, sign_to_id, id_to_tran):
-    print(compute_accuracy(test_texts, hmm_viterbi, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2))
-    print(compute_accuracy(test_texts, memm_greedy, logreg, vec, idx_to_tag_dict))
-    print(compute_accuracy(test_texts, BiLSTM_predict, model, predictor, sign_to_id, id_to_tran))
+    model, predictor, sign_to_id, id_to_tran = load_object_from_file(Path("../output/biLSTM_model.pkl"))
+    print(compute_accuracy(biLSTM_test_texts, BiLSTM_predict, model, predictor, sign_to_id, id_to_tran))
 
 
 def main():
-    test_texts = biLSTM_memm_and_hmm_train_and_store()
-    most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2, \
-    sign_to_id, tran_to_id, id_to_sign, id_to_tran, logreg, vec, idx_to_tag_dict, extra_decoding_arguments, \
-    model, predictor = load_trained_data()
-
-    test_trained_data(test_texts, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2, logreg, vec,
-                      idx_to_tag_dict, model, predictor, sign_to_id, id_to_tran)
+    hmm_train_and_test()
+    memm_train_and_test()
+    biLSTM_train_and_test()
 
 
 if __name__ == '__main__':
--- src/transliterate.py
+++ src/transliterate.py
@@ -1,12 +1,11 @@
-from data import logits_to_trans
+from pathlib import Path
+from data import logits_to_trans, load_object_from_file
 from combine_algorithms import overall_classifier, overall_choose_best_gammas, list_to_tran
-from train import sentence_to_allen_format, load_trained_data
+from train import sentence_to_allen_format
 
 
 def signs_to_transliteration(sentence):
-    most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2, \
-    sign_to_id, tran_to_id, id_to_sign, id_to_tran, logreg, vec, idx_to_tag_dict, extra_decoding_arguments, \
-    model, predictor = load_trained_data()
+    model, predictor, sign_to_id, id_to_tran = load_object_from_file(Path("../output/biLSTM_model.pkl"))
 
     tag_logits = predictor.predict(sentence_to_allen_format(sentence, sign_to_id, True))['tag_logits']
     biLSTM_predicted_tags, _, _, _, _, _ = logits_to_trans(tag_logits, model, id_to_tran)
@@ -14,9 +13,13 @@ def signs_to_transliteration(sentence):
 
 
 def main():
-    most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2, \
-    sign_to_id, tran_to_id, id_to_sign, id_to_tran, logreg, vec, idx_to_tag_dict, extra_decoding_arguments, \
-    model, predictor = load_trained_data()
+    most_common_tag, possible_tags, q, e, S, total_tokens, q_bi_counts, q_uni_counts, lambda1, lambda2 = \
+        load_object_from_file(Path(r"../output/hmm_model.pkl"))
+
+    logreg, vec, idx_to_tag_dict, extra_decoding_arguments = load_object_from_file(Path("../output/memm_model.pkl"))
+
+    model, predictor, sign_to_id, id_to_tran = load_object_from_file(Path("../output/biLSTM_model.pkl"))
+
     gamma1 = 0.4
     gamma2 = 0.2
 
