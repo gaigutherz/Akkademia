07d74dff27fea7efdbf70632c9ec11e1af60d1ba
gaigutherz <gaigutherz@gmail.con>
2020-10-25 01:29:04 +0200

    Add fairseq-train to bash file

--- create_linux_training_env.sh
+++ create_linux_training_env.sh
@@ -17,3 +17,15 @@ fairseq-preprocess \
     --source-lang ak --target-lang en \
     --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
     --destdir data-bin --thresholdtgt 0 --thresholdsrc 0 --workers 60
+
+# train
+mkdir -p checkpoints/fconv_ak_en
+fairseq-train \
+    data-bin \
+    --arch fconv \
+    --dropout 0.1 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --optimizer nag --clip-norm 0.1 \
+    --lr 0.5 --lr-scheduler fixed --force-anneal 50 \
+    --max-tokens 3000 \
+    --save-dir checkpoints/fconv_ak_en
