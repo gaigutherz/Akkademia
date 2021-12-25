4ae74be6257adc9eb1210a7413b887f67d904712
gaigutherz <gaigutherz@gmail.con>
2021-12-25 21:32:19 +0200

    Fix bugs in translate.py

--- akkadian/translate.py
+++ akkadian/translate.py
@@ -4,7 +4,7 @@ from translation_tokenize import tokenize
 
 
 def translate_from_transliteration(file):
-    tokenize("transliteration_bpe", file, False, "", "/tmp")
+    tokenize("transliteration_bpe", file, False, Path("NMT_input/tokenization"), "", "/tmp")
     cmd = "../../fairseq/fairseq_cli/interactive.py " \
           "../data-bin-transliteration/ " \
           "--path ../trans_result.LR_0.1.MAX_TOKENS_4000/checkpoint_best.pt " \
