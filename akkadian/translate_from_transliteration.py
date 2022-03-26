8aac11995867016748bb970e24aeacfffed432c8
gaigutherz <gaigutherz@gmail.con>
2022-03-26 19:16:22 +0300

    Add translate_from_cuneiform() and translate_from_transliteration() that prints only the relevant data for translation

--- akkadian/translate_from_transliteration.py
+++ akkadian/translate_from_transliteration.py
@@ -1,26 +1,29 @@
 import subprocess
 from pathlib import Path
 from translation_tokenize import tokenize
-from akkadian.translate_common import source_or_translation
+from translate_common import source_or_translation
 
 
-def translate_from_transliteration_base(file):
+def translate_from_transliteration_base(file, capture_output=False):
     tokenize("transliteration_bpe", file, False, Path("NMT_input/tokenization"), Path(""), Path("/tmp"))
     cmd = "../fairseq/fairseq_cli/interactive.py " \
           "data-bin-transliteration/ " \
           "--path trans_result.LR_0.1.MAX_TOKENS_4000/checkpoint_best.pt " \
           "--beam 5 " \
           "--input /tmp/" + file
+
+    if capture_output:
+        return subprocess.run(cmd.split(), capture_output=True)
     return subprocess.run(cmd.split())
 
 
 def translate_from_transliteration_raw(file):
-    print(translate_from_transliteration_base(file).stdout)
+    translate_from_transliteration_base(file)
 
 
 def translate_from_transliteration(file):
-    raw_result = translate_from_transliteration_base(file).stdout
-    for line in raw_result:
+    raw_result = translate_from_transliteration_base(file, True).stdout
+    for line in raw_result.decode().split('\n'):
         if not source_or_translation(line):
             continue
         print(line)
