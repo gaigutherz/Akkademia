f077d3aac4ccb9638a0663c0ddc5fde58a734f3f
gaigutherz <gaigutherz@gmail.con>
2022-06-17 18:51:18 +0300

    Insert an option for transliteration input preprocessing

--- akkadian/translate_from_transliteration.py
+++ akkadian/translate_from_transliteration.py
@@ -4,13 +4,30 @@ from translation_tokenize import tokenize
 from translate_common import source, translation, detokenize_transliteration, detokenize_translation
 
 
+def organize_transliteration_line(line):
+    return line
+
+
+def organize_transliteration_input(file):
+    tmp_file_name = "tmp_" + file
+    tmp_file = open(tmp_file_name, "w", encoding="utf8")
+
+    with open(file, "r", encoding="utf8"):
+        for line in file:
+            tmp_file.write(organize_transliteration_line(line))
+
+    tmp_file.close()
+    return tmp_file_name
+
+
 def translate_transliteration_base(file, capture_output=False):
-    tokenize("transliteration_bpe", file, False, Path("NMT_input/tokenization"), Path(""), Path("/tmp"))
+    tmp_file = organize_transliteration_input(file)
+    tokenize("transliteration_bpe", tmp_file, False, Path("NMT_input/tokenization"), Path(""), Path("/tmp"))
     cmd = "../fairseq/fairseq_cli/interactive.py " \
           "data-bin-transliteration/ " \
           "--path trans_result.LR_0.1.MAX_TOKENS_4000/checkpoint_best.pt " \
           "--beam 5 " \
-          "--input /tmp/" + file
+          "--input /tmp/" + tmp_file
 
     if capture_output:
         return subprocess.run(cmd.split(), capture_output=True)
