715ec7ac5f787fcb12350e5b80f70b169440415d
gaigutherz <gaigutherz@gmail.con>
2022-06-26 00:48:12 +0300

    Add fix_numbers() and fix_acute_grave() to organize_transliteration_line()

--- akkadian/translate_from_transliteration.py
+++ akkadian/translate_from_transliteration.py
@@ -1,22 +1,35 @@
 import subprocess
 from pathlib import Path
+import string
 from translation_tokenize import tokenize
 from translate_common import source, translation, detokenize_transliteration, detokenize_translation
 
-letter_substitutions = {"ḫ": "h",
+letter_substitutions = {
+    "ḫ": "h"
+}
+
+number_substitutions = {
+    "0": "₀", "1": "₁", "2": "₂", "3": "₃", "4": "₄", "5": "₅", "6": "₆", "7": "₇", "8": "₈", "9": "₉"
+}
+
+acute_grave_substitutions = {
     "á": "a₂", "é": "e₂", "í": "i₂", "ó": "o₂", "ú":"u₂",
-    "à": "a₃", "è": "e₃", "ì": "i₃", "ò": "o₃", "ù":"u₃",
-    "0": "₀", "1": "₁", "2": "₂", "3": "₃", "4": "₄", "5": "₅", "6": "₆", "7": "₇", "8": "₈", "9": "₉"}
+    "à": "a₃", "è": "e₃", "ì": "i₃", "ò": "o₃", "ù":"u₃"
+}
 
-logogram_substitutions = {"--": "-", "- ": "-",
-                          "{KI}-": "{KI} ", "{M}": "{m}", "{D}": "{d}",
-                          "₂}": "2}", "₃}": "3}"}
+logogram_substitutions = {
+    "--": "-", "- ": "-",
+    "{KI}-": "{KI} ", "{M}": "{m}", "{D}": "{d}",
+    "₂}": "2}", "₃}": "3}"
+}
 
-exception_substitutions = {"aš₂": "aš2", "kas₂": "kas2", "kul₂": "kul2", "dab₂": "dab2",
-                        "šul₃": "šul3", "ti₃": "ti3", "kat₃": "kat3", "lib₃": "lib3",
-                        "tu₄": "tu4", "u₄": "u4",
-                        "LIL2": "LIL₂", "DU8": "DU₈", "LU2": "LU₂", "ŠA3": "ŠA₃",
-                           "ŠAR2": "ŠAR₂", "SIG5": "SIG₅", "Ku3": "KU₃", "Du3": "DU₃"}
+exception_substitutions = {
+    "aš₂": "aš2", "kas₂": "kas2", "kul₂": "kul2", "dab₂": "dab2",
+    "šul₃": "šul3", "ti₃": "ti3", "kat₃": "kat3", "lib₃": "lib3",
+    "tu₄": "tu4", "u₄": "u4",
+    "LIL2": "LIL₂", "DU8": "DU₈", "LU2": "LU₂", "ŠA3": "ŠA₃",
+    "ŠAR2": "ŠAR₂", "SIG5": "SIG₅", "Ku3": "KU₃", "Du3": "DU₃"
+}
 
 def find_all_occurences(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
@@ -58,6 +71,44 @@ def substitute_phrase(line, substitution_dict):
     return line
 
 
+def is_constant(l):
+    if l in string.ascii_letters and not l in "aeiouAEIOU":
+        return True
+    return False
+
+
+def fix_acute_grave(line):
+    new_line = ""
+
+    i = 0
+    while i < len(line):
+        if line[i] in acute_grave_substitutions:
+            if i + 1 < len(line) and is_constant(line[i+1]):
+                new_line += acute_grave_substitutions[line[i]][0] + line[i+1] + acute_grave_substitutions[line[i]][1]
+                i += 1
+            else:
+                new_line += acute_grave_substitutions[line[i]]
+        else:
+            new_line += line[i]
+        i += 1
+
+    return new_line
+
+
+def fix_numbers(line):
+    new_line = ""
+
+    prev_l = ""
+    for l in line:
+        if prev_l and prev_l in string.ascii_letters and l in number_substitutions:
+            new_line += number_substitutions[l]
+        else:
+            new_line += l
+        prev_l = l
+
+    return new_line
+
+
 def organize_transliteration_line(line):
     new_line = ""
 
@@ -67,6 +118,8 @@ def organize_transliteration_line(line):
         else:
             new_line += l
 
+    new_line = fix_numbers(new_line)
+    new_line = fix_acute_grave(new_line)
     new_line = fix_logogram(new_line)
     new_line = substitute_phrase(new_line, exception_substitutions)
 
